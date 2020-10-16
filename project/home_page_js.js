96cbac4854dd611e5aef3547557179892d3f076e
harshavb <48032800+harshavb@users.noreply.github.com>
2020-10-16 10:01:52 -0700

    Update home_page_js.js

--- project/home_page_js.js
+++ project/home_page_js.js
@@ -1,10 +1,16 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	fill(81);
-	circle(windowWidth/2, windowHeight/2, windowHeight/2);
 }
 
 function draw() {
+	var context = p5.drawingContext;
+	fill(25, 154, 75);
+	beginShape();
+	vertex(0, 2 * windowHeight/7);
+	vertex(windowWidth/3, 2 * windowHeight/7);
+	bezierVertex(windowWidth/3 + windowWidth/14, 2 * windowHeight/7, windowWidth/3 + windowWidth/14, 3 * windowHeight/7, windowWidth/3, 3 * windowHeight/7);
+	vertex(0, 3 * windowHeight/7);
+	endShape(CLOSE);
 }
 
 function windowResized() {
