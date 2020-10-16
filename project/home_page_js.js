9e0c071b1ecd67e5fac19fa0db0e7ee49f63a3fe
harshavb <48032800+harshavb@users.noreply.github.com>
2020-10-16 11:05:38 -0700

    Update home_page_js.js

--- project/home_page_js.js
+++ project/home_page_js.js
@@ -1,5 +1,6 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	drawingContext.shadowColor = 'black';
 	strokeWeight(3);
 	fill(32, 63, 153);
 	rect(0, 0, windowWidth, windowHeight/6);
@@ -10,7 +11,6 @@ function setup() {
 	fill(48, 45, 46);
 	rect(0, 6 * windowHeight/7, windowWidth, windowHeight/7);
 	drawingContext.shadowBlur = 5;
-	drawingContext.shadowColor = 'black';
 	drawingContext.shadowOffsetX = 2;
 	drawingContext.shadowOffsetY = 2;
 	fill(25, 154, 75);
@@ -24,6 +24,9 @@ function draw() {
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+	drawingContext.shadowBlur = 0;
+	drawingContext.shadowOffsetX = 0;
+	drawingContext.shadowOffsetY = 0;
 	strokeWeight(3);
 	fill(32, 63, 153);
 	rect(0, 0, windowWidth, windowHeight/6);
@@ -33,6 +36,9 @@ function windowResized() {
 	stroke('black');
 	fill(48, 45, 46);
 	rect(0, 6 * windowHeight/7, windowWidth, windowHeight/7);
+	drawingContext.shadowBlur = 5;
+	drawingContext.shadowOffsetX = 2;
+	drawingContext.shadowOffsetY = 2;
 	fill(25, 154, 75);
 	rect(0, 2 * windowHeight/7, 2 * windowWidth/5, windowHeight/7, 0, windowWidth/14, windowWidth/14, 0);
 	rect(3 * windowWidth/5, 3 * windowHeight/7, 2 * windowWidth/5, windowHeight/7, windowWidth/14, 0, 0, windowWidth/14);
