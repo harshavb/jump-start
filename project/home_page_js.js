bbeefaf489757d08eee219102d38652192be4ade
harshavb <48032800+harshavb@users.noreply.github.com>
2020-10-16 09:40:51 -0700

    Basic Setup

---
+++ project/home_page_js.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	fill(81);
+	circle(windowWidth/2, windowHeight/2, windowHeight/2);
+}
+
+function draw() {
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
